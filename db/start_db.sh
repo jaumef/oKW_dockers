1daec5d9588c987bc563bb86b9f764a29885aeaa
Jaume Florez <jflorez@gisce.net>
2017-03-22 10:41:13 +0100

    Added start script theat builds and runs db

---
+++ db/start_db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+container="db"
+image="okw/"$container
+docker rmi $image
+docker build -t $image .
+ports="27017:27017 -p 6379:6379"
+docker stop $container
+docker rm $container
+container_id=`docker run -d -p $ports --name $container -i $image`
+if [ "$container_id" != "" ]
+then
+    echo "$container_id" > id_db
+    echo "Container started! id: "$container_id
+else
+    if [ -e "id_frontend" ]; then
+        rm id_db
+    fi
+    echo "Error spawning docker!"
+fi
