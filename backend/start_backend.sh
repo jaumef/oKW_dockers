05b7e0128e3806919ec4c6abe0054c8a4f1804a3
Jaume Florez <jflorez@gisce.net>
2017-03-24 14:55:18 +0100

    Update start backend script

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 container="backend"
 image="okw/"$container
-docker rmi $image
+#docker rmi $image
 docker build -t $image --network host .
 ports="5000:5000"
-docker stop $container
-docker rm $container
-container_id=`docker run -d -p $ports --name $container -i $image --link db`
+./stop_backend.sh
+container_id=`docker run -d -p $ports --name $container -i $image`
+echo "/!\\ ENTRYPOINT NOT SET, NOTHING IS RUNNING IN $container_id"
 if [ "$container_id" != "" ]
 then
     echo "$container_id" > id_backend
