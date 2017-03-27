3369876f3575866f6b131b9b1b52ddf96d32fb1f
Jaume Florez <jflorez@gisce.net>
2017-03-27 11:19:47 +0200

    Add build from dockerfile and socket sharing

--- frontend/start_frontend.sh
+++ frontend/start_frontend.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "Using container name: 'frontend'"
 name="frontend"
+container="okw/"$name
 echo "Using default ports (8080->80 443->443)"
-ports="8080:80 -p 443:443"
+ports="-p 8080:80 -p 443:443"
 echo "Using as default html path: {cwd}/html"
 html_path=$PWD"/html"
 echo "Using as default nginx conf: {cwd}/nginx.conf"
 nginx_config_path=$PWD"/nginx.conf"
-container_id=`docker run -p $ports --name $name -d -v $html_path:/usr/share/nginx/html -v $nginx_config_path:/etc/nginx/sites-enabled/default.conf nginx`
+docker build -t $container . 
+container_id=`docker run $ports --name $name -d -i $container -v /tmp/docker_sockets:/tmp/`
 if [ "$container_id" != "" ]
 then
     echo "$container_id" > id_frontend
