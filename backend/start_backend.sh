2204da4f5c490d4c9ed03f0fb47b9cf7809710bf
Jaume Florez <jflorez@gisce.net>
2017-03-28 12:07:28 +0200

    Update backend start script

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -4,8 +4,14 @@ image="okw/"$container
 #docker rmi $image
 docker build -t $image --network host .
 ports="-p 5000:5000"
+# Ensure sockets directory exist
+sockets_path="/tmp/docker_sockets"
+sockets="-v $sockets_path:/tmp/"
+if [ -d $sockets_path ]; then
+    mkdir -p $sockets_path
+fi
 ./stop_backend.sh
-container_id=`docker run -d $ports --name $container -v /tmp/docker_sockets:/tmp/ --network host -i $image`
+container_id=`docker run -d $ports --name $container $sockets --network host -i $image`
 if [ "$container_id" != "" ]
 then
     echo "$container_id" > id_backend
@@ -16,3 +22,5 @@ else
     fi
     echo "Error spawning docker!"
 fi
+# Enable writing on socket
+chmod 777 $sockets_path/orakwlum-api.sock
